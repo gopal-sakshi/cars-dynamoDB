53330d86f34ad951291a19b72387cc8c3ce5c1e9
gopal612 <gopal612@gmail.com>
2024-11-21 22:08:52 +0530

    switch local & AWS

--- dynamoPractice/query_base_onlineshop.js
+++ dynamoPractice/query_base_onlineshop.js
@@ -1,6 +1,17 @@
-var { dynamoClient } = require("../config/aws-config-v3")
-var { QueryCommand, DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
-const docClient23 = DynamoDBDocumentClient.from(dynamoClient);
+var fs = require("fs"), dynamoClient34;
+if(fs.existsSync("config/aws-config-v3")) {
+    dynamoClient34 = require("../config/aws-config-v3")
+} else {
+    const { fromIni } = require("@aws-sdk/credential-providers");
+    var { QueryCommand, DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+    const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+    var credentials34 = fromIni({
+        profile: "priya22",
+    });
+    dynamoClient34 = new DynamoDBClient({ region: 'us-east-1', credentials: credentials34 });
+}
+
+const docClient23 = DynamoDBDocumentClient.from(dynamoClient34);
 
 module.exports = async (params) => {
     const command = new QueryCommand(params);
