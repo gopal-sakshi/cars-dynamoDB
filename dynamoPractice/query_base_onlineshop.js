2fe866646ee6dc24927cd9dbd6c529456b058777
gopal612 <gopal612@gmail.com>
2024-11-21 22:20:18 +0530

    switch local & prod

--- dynamoPractice/query_base_onlineshop.js
+++ dynamoPractice/query_base_onlineshop.js
@@ -1,9 +1,13 @@
-var fs = require("fs"), dynamoClient34;
-if(fs.existsSync("config/aws-config-v3")) {
-    dynamoClient34 = require("../config/aws-config-v3")
+var { QueryCommand, DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+var fs = require("fs"); 
+var dynamoClient34;
+
+if(fs.existsSync("config/aws-config-v3.js")) {
+    console.log("using dynamo LOCALLLLLL -- else block uses priya22 AWS prod");
+    let { dynamoClient } = require("../config/aws-config-v3");
+    dynamoClient34 = dynamoClient;
 } else {
-    const { fromIni } = require("@aws-sdk/credential-providers");
-    var { QueryCommand, DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+    const { fromIni } = require("@aws-sdk/credential-providers");    
     const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
     var credentials34 = fromIni({
         profile: "priya22",
